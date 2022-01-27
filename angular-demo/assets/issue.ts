038dc1b41b8d661b36c426f16b84d174d0a6931b
zyxbest <48346252+zyxbest@users.noreply.github.com>
2022-01-27 11:43:57 +0000

    Deploying to gh-pages from @ zyxbest/angular-demo@1436eaf27789027e6a17f0556f329a7f22312696 🚀

---
+++ angular-demo/assets/issue.ts
@@ -0,0 +1,4 @@
+export const ISSUE_BASIC = {
+  owner: 'zyxbest',
+  repo: 'db',
+};
---
+++ angular-demo/assets/path.ts
@@ -0,0 +1,7 @@
+export enum Path {
+  LOGIN_ROUTE = 'unauth/login',
+}
+export enum URL {
+  GITHUB_AUTHORIZE = 'https://github.com/login/oauth/authorize',
+  GITHUB_ACCESSTOKEN = 'https://github.com/login/oauth/access_token',
+}
